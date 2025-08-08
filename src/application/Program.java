6074620918e824b5591bb3d568461a07c2c128a4
Hiran Ferreti Baccos <hiranfbcj@outlook.com>
2025-08-08 19:33:57 -0300

    first commit

---
+++ src/application/Program.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import entities.Product;
+
+public class Program {
+	public static void main(String[] args) {
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Notebook", 1200.00));
+		list.add(new Product("Tablet", 450.00));
+
+		Collections.sort(list);
+
+		for (Product p : list) {
+			System.out.println(p);
+		}
+	}
+}
